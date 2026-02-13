07893dbbd51a3150db3b07cdd337cd34eb174683
Sweta8904 <swetaramesh.tk@gmail.com>
2026-02-13 18:36:06 +0530

    Add final comit

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,5 +3,6 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+console.log("ENV API URL:", import.meta.env.VITE_API_URL);
 
 export default API;
